1dbb5a5523c8e9801856eb3be831a7fd56608790
mathieurella <mathieurella@outlook.fr>
2016-10-12 15:30:35 +0200

    form complet

--- form.php
+++ form.php
@@ -4,15 +4,46 @@
 if(!empty($_POST['submit']))
 
 {
-    $f_projet = $_POST['projet'];
-    $f_client = $_POST['client'];
+    $f_projet = $_POST['Projet'];
+    $f_nom = $_POST['t1'];
+    $f_prenom = $_POST['t2'];
+    $f_adresse = $_POST['t3'];
+    $f_mail = $_POST['Mail'];
+    $f_site = $_POST['Site'];
+    $f_objectifSmart = $_POST['ObjectifSmart'];
 
     require("fpdf/fpdf.php");
     $pdf = new FPDF ();
     $pdf->AddPage();
     $pdf->SetFont("Arial","B",16);
-    $pdf->Cell(0,10,"welcome {$f_projet}",1,0,C);
-    $pdf->Cell(50,10,"{$f_client}",0,0);
+    $pdf->Cell(0,10,"Projet {$f_projet}",1,0,C);
+
+    $pdf->Ln(14);
+
+    $pdf->Cell(0,10,"Information client",1,0,C);
+    $pdf->Ln(10);
+    $pdf->Cell(50,10,"Nom :",1,0);
+    $pdf->Cell(50,10,$f_nom,1,0);
+    $pdf->Ln(10);
+    $pdf->Cell(50,10,"Prenom :",1,0);
+    $pdf->Cell(50,10,$f_prenom,1,0);
+    $pdf->Ln(10);
+    $pdf->Cell(50,10,"Adresse :",1,0);
+    $pdf->Cell(50,10,$f_adresse,1,0);
+    $pdf->Ln(10);
+    $pdf->Cell(50,10,"Mail :",1,0);
+    $pdf->Cell(50,10,$f_mail,1,0);
+    $pdf->Ln(10);
+    $pdf->Cell(50,10,"Site :",1,0);
+    $pdf->Cell(50,10,$f_site,1,0);
+
+    $pdf->Ln(14);
+    $pdf->Cell(0,10,"Objectif Smart",1,0,C);
+    $pdf->Ln(10);
+    $pdf->Cell(0,10,$f_objectifSmart,1,0);
+
     $pdf->output();
+
+
 }
 ?>
